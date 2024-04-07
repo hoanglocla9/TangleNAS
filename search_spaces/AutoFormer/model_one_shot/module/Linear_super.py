531c01e359bbdd7a1f46f5cd972c3d470797bb81
RHEA SUKTHANKER <sukthank@RHEAs-MacBook-Pro-2.local>
2024-04-07 17:10:34 +0200

    update and minor fixes

--- search_spaces/AutoFormer/model_one_shot/module/Linear_super.py
+++ search_spaces/AutoFormer/model_one_shot/module/Linear_super.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
+import math
 
 def gelu(x: torch.Tensor) -> torch.Tensor:
     if hasattr(torch.nn.functional, 'gelu'):
@@ -88,3 +88,117 @@ class LinearSubRatioEmb(nn.Module):
             if x.shape[-1] != self.super_emb:
                 x = F.pad(x, (0, self.super_emb - x.shape[-1]), "constant", 0)
         return x
+
+import itertools
+class LinearSubRatioMixture(nn.Module):
+
+    def __init__(self,
+                 layer: torch.nn.Linear,
+                 emb_choice_list: list,
+                 ratio_choice_list: list,
+                 reverse: bool = False) -> None:
+        super(LinearSubRatioMixture, self).__init__()
+
+        # super_in_dim and super_out_dim indicate the largest network!
+        self.layer = layer
+        self.emb_choice_list = emb_choice_list
+        self.ratio_choice_list = ratio_choice_list
+        self.emb_ratio_choice_list = list(itertools.product(self.emb_choice_list, self.ratio_choice_list))
+        self.reverse = reverse
+        self.max_dim = int(max(self.emb_choice_list)*max(self.ratio_choice_list))
+        self.max_emb = max(self.emb_choice_list)
+        #self.activation_fn = gelu
+        self.layer = layer
+
+    def _compute_weight_and_bias(self, weights, idx, conv_weight, conv_bias, op_weight, op_bias, use_argmax=False):
+        alpha = weights[idx]
+        emb_size, ratio = self.emb_ratio_choice_list[idx]
+
+        #print(op_weight.shape)
+        if not self.reverse:
+            weight_curr = alpha * op_weight[:int(emb_size * ratio
+                                            ), :emb_size]
+            if not use_argmax:
+                conv_weight += F.pad(weight_curr, (0,self.max_emb-emb_size,0,self.max_dim-int(emb_size * ratio)), "constant", 0)
+            else:
+                conv_weight = weight_curr
+            if op_bias is not None:
+                bias = alpha * op_bias[:int(emb_size * ratio)]
+                if not use_argmax:
+                    conv_bias += F.pad(bias,(0,self.max_dim-int(emb_size * ratio)),"constant",0)
+                else:
+                    conv_bias = bias
+
+        else:
+            weight_curr = alpha * op_weight[:emb_size, :int(emb_size * ratio
+                                            ),]
+            if not use_argmax:
+                conv_weight += F.pad(weight_curr, (0,self.max_dim-int(emb_size * ratio),0,self.max_emb-emb_size), "constant", 0)
+            else:
+                conv_weight = weight_curr
+            if op_bias is not None:
+                bias = alpha * op_bias[:emb_size]
+            if not use_argmax:
+                conv_bias += F.pad(bias,(0,self.max_emb-emb_size),"constant",0)     
+            else:
+                conv_bias = bias    
+
+        return conv_weight, conv_bias
+
+    def compute_weight_and_bias_mixture(self, weights, op_weight, op_bias, use_argmax=False):
+        conv_weight = 0
+        conv_bias = 0
+        if use_argmax == True:
+            argmax = np.array([w.item() for w in weights]).argmax()
+            conv_weight, conv_bias = self._compute_weight_and_bias(
+                weights=weights,
+                idx=argmax,
+                conv_weight=conv_weight,
+                conv_bias=conv_bias,
+                op_weight=op_weight,
+                op_bias=op_bias,
+                use_argmax=use_argmax
+            )
+        else:
+            for i, _ in enumerate(weights):
+                conv_weight, conv_bias = self._compute_weight_and_bias(
+                    weights=weights,
+                    idx=i,
+                    conv_weight=conv_weight,
+                    conv_bias=conv_bias,
+                    op_weight=op_weight,
+                    op_bias=op_bias,
+                    use_argmax=use_argmax
+                )
+        if op_bias==None:
+            conv_bias = op_bias
+        return conv_weight, conv_bias
+
+    def forward(self, x: torch.Tensor, weights: torch.Tensor, use_argmax=False) -> torch.Tensor:
+        weight, bias = self.compute_weight_and_bias_mixture(weights,self.layer.weight, self.layer.bias, use_argmax=use_argmax)
+        #print(weight)
+        #print(bias)
+        argmax = np.array([w.item() for w in weights]).argmax()
+        emb_size, ratio = self.emb_ratio_choice_list[argmax]
+        if use_argmax:
+            if not self.reverse:
+                x = F.linear(x[:, :, :int(emb_size)], weight, bias)
+            else:
+                x = F.linear(x[:, :, :int(emb_size * ratio)], weight, bias)
+        else:
+            x = F.linear(x, weight, bias)
+        return x
+    
+# test linear sub ratio mixture
+'''if __name__ == "__main__":
+    layer = torch.nn.Linear(12, 3)
+    layer.weight.data = torch.ones_like(layer.weight.data)
+    layer.bias.data = torch.ones_like(layer.bias.data)
+    layer = LinearSubRatioMixture(layer, [1,2,3], [2,3,4], reverse=True)
+    x = torch.ones((1,1,12))
+    weights = torch.tensor([0.1,0.2,0.3,0.4,0.5,0.9,0.7,0.8,0.6], requires_grad=True)
+    y = layer(x, weights, use_argmax=False)
+    print(y.shape)
+    print(y)
+    y.sum().backward()
+    print(weights.grad)'''
